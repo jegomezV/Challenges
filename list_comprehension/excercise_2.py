9d8cc9c68f56c10e8679bce1f2fe12d9f27e89d3
jegomezV <jegomezvalencia@gmail.com>
2023-09-07 12:20:25 -0500

    correct_excercise

--- list_comprehension/excercise_2.py
+++ list_comprehension/excercise_2.py
@@ -26,24 +26,13 @@ This script defines a function, dict_create, which takes a list of numbers and c
 # Import the random module to generate random numbers
 import random
 
+
 # Define a function called dict_create that takes a list of numbers as input
 def dict_create():
-    # Create an empty dictionary
-    result_dict = {}
+    # Create a dictionary using a dictionary comprehension
+    # The keys are the numbers from list_random_nums
+    # The values are "even" (even) or "odd" (odd) based on a conditional expression
+    print({num: "even" if num % 2 == 0 else "odd" for num in [random.randint(0, 100) for _ in range(10)]})
 
-    # Generate a list of 10 random numbers
-    random_nums = [random.randint(0, 100) for _ in range(10)]
-
-    # Loop through the random numbers
-    for num in random_nums:
-        # Check if the number is even or odd and add it to the dictionary
-        if num % 2 == 0:
-            result_dict[num] = "even"
-        else:
-            result_dict[num] = "odd"
-    
-    # Print the resulting dictionary
-    print(result_dict)
-
-# Call the dict_create function
+# Call the dict_create function with the list of random numbers as an argument
 dict_create()
